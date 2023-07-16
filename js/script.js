0b80b8def7dc32427c146463c2e9dfaff3fc388b
CrazyCat7100 <134160263+CrazyCat7100@users.noreply.github.com>
2023-07-16 18:55:23 +0000

    .

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// let name = 'Alex';
+
+// let mainPhoto = document.getElementsByClassName('main__photo')[0]
+// let mainSubtitle = document.getElementsByClassName('main__subtitle')[0]
+
+// mainPhoto.addEventListener('click', function () {
+//     mainPhoto.style.transform = '100px'
+// })
