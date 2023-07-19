fedeb2c32b86b3c7303ef0ffd2c6e1311a4d7e54
CrazyCat7100 <134160263+CrazyCat7100@users.noreply.github.com>
2023-07-19 14:11:47 +0000

    .

--- py/main.py
+++ py/main.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
+
+@app.route('/math')
+def math():
+    return render_template('math.html')
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
