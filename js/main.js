14fae0dd350d5ae80463941d51f8c5f10b82ace6
CrazyCat7100 <134160263+CrazyCat7100@users.noreply.github.com>
2023-09-30 19:24:39 +0000

    .

--- js/main.js
+++ js/main.js
@@ -6,3 +6,9 @@ cookieClicker.addEventListener('click', function () {
     clicks += 1
     cookieCounter.textContent = clicks
 })
+
+let home = document.getElementsByTagName('button')[0]
+
+home.addEventListener('click', function () {
+    location.href = 'index.html'
+})
