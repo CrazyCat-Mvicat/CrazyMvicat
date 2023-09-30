d831225d2e2fa12a0e350147bfc95c0ebfc33071
CrazyCat7100 <134160263+CrazyCat7100@users.noreply.github.com>
2023-09-30 16:24:10 +0000

    .

--- js/script.js
+++ js/script.js
@@ -1,21 +1,5 @@
-// let name = 'Alex';
+let haveFun = document.getElementsByTagName('button')[0]
 
-// let mainPhoto = document.getElementsByClassName('main__photo')[0]
-// let mainSubtitle = document.getElementsByClassName('main__subtitle')[0]
-
-// mainPhoto.addEventListener('click', function () {
-//     mainPhoto.style.transform = '100px'
-// })
-const a = 500; // variable (not change)
-
-
-let notification = document.getElementsByClassName("notification")[0];
-
-notification.addEventListener('click', function () {
-    notification.style.opacity = 0;
-    fetch("http://127.0.0.1:3002/delete-notification")
-        .then((res)=>(res.json()))
-        .then(json=>{
-          // json {result: 200} {error: 400} python/c#/php
-        })
+haveFun.addEventListener('click', function () {
+    location.href = 'main.html'
 })
