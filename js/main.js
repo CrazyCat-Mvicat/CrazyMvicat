722a6e58ed6f7357b759fd422909ee872ce81be9
CrazyCat7100 <134160263+CrazyCat7100@users.noreply.github.com>
2023-10-01 19:01:05 +0000

    .

--- js/main.js
+++ js/main.js
@@ -2,9 +2,18 @@ let cookieClicker = document.getElementsByClassName('cookie_clicker')[0]
 let cookieCounter = document.getElementsByClassName('cookie_counter')[0]
 let clicks = 0
 
-cookieClicker.addEventListener('click', function () {
+cookieClicker.addEventListener('click', function (e) {
     clicks += 1
-    cookieCounter.textContent = clicks
+    cookieCounter.textContent = (clicks + ' Cookies')
+    let h3 = document.createElement('h3')
+    h3.textContent = '+ 1'
+    h3.classList.add('plusone')
+    h3.style.left = e.clientX + 'px'
+    h3.style.top = e.clientY + 'px'
+    document.body.append(h3)
+    setTimeout(function () {
+        h3.remove()
+    }, 2000)
 })
 
 let home = document.getElementsByTagName('button')[0]
