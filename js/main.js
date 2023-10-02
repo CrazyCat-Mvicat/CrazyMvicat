08b1e7d602adb5b6d3838538dc035ccc1008cb11
CrazyCat7100 <134160263+CrazyCat7100@users.noreply.github.com>
2023-10-02 00:37:04 +0000

    .

--- js/main.js
+++ js/main.js
@@ -16,8 +16,64 @@ cookieClicker.addEventListener('click', function (e) {
     }, 2000)
 })
 
+// -------------------------------------------------------------- //
+
 let home = document.getElementsByTagName('button')[0]
 
 home.addEventListener('click', function () {
     location.href = 'index.html'
 })
+
+// --------------------------------------------------------------- //
+
+let td = document.getElementsByTagName("td");
+let result = document.getElementsByClassName("result")[0];
+let isX = true;
+let endGame = false;
+let playAgain = document.getElementsByClassName("play_again")[0];
+let combinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [0, 4, 8],
+];
+
+function winCheck() {
+  for (let i = 0; i < combinations.length; i++) {
+    if (
+      td[combinations[i][0]].textContent != "" &&
+      td[combinations[i][0]].textContent ==
+        td[combinations[i][1]].textContent &&
+      td[combinations[i][1]].textContent &&
+      td[combinations[i][2]].textContent
+    ) {
+      // alert("win " + td[combinations[i][0]].textContent);
+      result.textContent = td[combinations[i][0]].textContent + " Wins!";
+      endGame = true;
+    }
+  }
+}
+
+for (let i = 0; i < 9; i++) {
+  td[i].addEventListener("click", function () {
+    if (td[i].textContent == "" && endGame == false) {
+      if (isX) {
+        td[i].textContent = "X";
+        isX = false;
+      } else {
+        td[i].textContent = "O";
+        isX = true;
+      }
+      winCheck();
+    }
+  });
+}
+
+playAgain.addEventListener("click", function () {
+  location.reload();
+});
+
